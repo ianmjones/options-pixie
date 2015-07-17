eb2f5df2f5602126a8e91ebfa16ecb3e358a94d1
Ian M. Jones <ian@ianmjones.com>
2015-07-18 01:37:00 +1200

    Added minimal config for Scrutinizer CI and a little extra info to repo readme.

--- tests/bin/run-travis-unittests.sh
+++ tests/bin/run-travis-unittests.sh
@@ -8,6 +8,12 @@ RUN_CMD="tests/bin/run-unittests.sh -d wordpress_test -u travis -h 127.0.0.1"
 if [ "3.9" == "${WP_VERSION}" -a "0" == "${WP_MULTISITE}" -a "5.3" == "${TRAVIS_PHP_VERSION}" ]
 then
 	bash ${RUN_CMD} -c /tmp/clover.xml
+
+	# Send coverage to Scrutinizer CI.
+	curl -sSL https://scrutinizer-ci.com/ocular.phar -o ocular.phar
+	php ocular.phar code-coverage:upload --format=php-clover /tmp/clover.xml
+
+	# Quick check that coverage hasn't dropped.
 	tests/bin/coverage-checker.php /tmp/clover.xml 5
 else
 	bash ${RUN_CMD}
--- tests/cfg/bootstrap.php
+++ tests/cfg/bootstrap.php
@@ -13,11 +13,11 @@ function _manually_load_plugin() {
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-function _manually_load_options_pixiep_plugin() {
+function _manually_load_options_pixie_plugin() {
 	require dirname( __FILE__ ) . '/../../src/options-pixie.php';
 }
 
-tests_add_filter( 'muplugins_loaded', '_manually_load_options_pixiep_plugin' );
+tests_add_filter( 'muplugins_loaded', '_manually_load_options_pixie_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
 
