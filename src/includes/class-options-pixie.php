80a601aa0398a2829d99ea40d8244f42f458eb7d
Ian M. Jones <ian@ianmjones.com>
2015-08-09 00:14:35 +1200

    WP 4.3 now using H1 tag for admin page titles.

--- src/includes/class-options-pixie.php
+++ src/includes/class-options-pixie.php
@@ -69,7 +69,7 @@ class Options_Pixie {
 	public function __construct() {
 
 		$this->options_pixie = 'options-pixie';
-		$this->version       = '1.0.1';
+		$this->version       = '1.0.2-dev';
 
 		$this->load_dependencies();
 		$this->set_locale();
